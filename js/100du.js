288bd99a4d5b863540390d33fadb60fa8d184031
maiboy <iamdbd@163.com>
2017-08-16 15:49:21 +0800

    init

---
+++ js/100du.js
@@ -0,0 +1,214 @@
+$(function(){
+  //搜索切换
+  (function(){
+    var aLi = $('#menu li');
+    var arrText = [
+      '例如：荷棠鱼坊烧鱼 或 樱花日本料理',
+      '例如：昌平区育新站龙旗广场2号楼609室',
+      '例如：万达影院双人情侣券',
+      '例如：东莞出事了，大老虎是谁？',
+      '例如：北京初春降雪，天气变幻莫测'
+    ];
+    var iNow = 0;
+    var oVal = $('#search').find('.form .text');
+    oVal.val(arrText[iNow]);
+    aLi.each(function(index) {
+      $(this).click(function(){
+        //console.log(index);
+        aLi.attr('class','gradient');
+        $(this).attr('class','active');
+        iNow = index;
+        oVal.val(arrText[iNow]);
+      })
+    });
+    oVal.focus(function() {
+      if ($(this).val() == arrText[iNow]) {
+        $(this).val('');
+      }
+    });
+    oVal.blur(function() {
+      if ($(this).val() == '') {
+        oVal.val(arrText[iNow]);
+      }
+    });
+
+  })();
+
+  //update文字滚动
+  (function(){
+    var oDiv = $('.update');
+    var oUl = oDiv.find('ul');
+    var iH = 0;
+    var arrData = [
+      { 'name':'萱萱', 'time':4, 'title':'那些灿烂华美的瞬间', 'url':'http://www.maiduo.ren' },
+      { 'name':'畅畅', 'time':5, 'title':'广东3天抓获涉黄疑犯', 'url':'http://www.maiduo.ren' },
+      { 'name':'萱萱', 'time':6, 'title':'国台办回应王郁琦', 'url':'http://www.maiduo.ren' },
+      { 'name':'畅畅', 'time':7, 'title':'那些灿烂华美的瞬间', 'url':'http://www.maiduo.ren' },
+      { 'name':'萱萱', 'time':8, 'title':'那些灿烂华美的瞬间', 'url':'http://www.maiduo.ren' },
+      { 'name':'畅畅', 'time':9, 'title':'广东3天抓获涉黄疑犯', 'url':'http://www.maiduo.ren' },
+      { 'name':'萱萱', 'time':10, 'title':'国台办回应王郁琦', 'url':'http://www.maiduo.ren' },
+      { 'name':'畅畅', 'time':11, 'title':'那些灿烂华美的瞬间', 'url':'http://www.maiduo.ren' }
+    ];
+    var str = '';
+    var oBtnUp = $('#updateUpBtn');
+    var oBtnDown = $('#updateDownBtn');
+    var iNow = 0;
+    var timer = null;
+    for (var i = 0; i < arrData.length; i++) {
+      str += '<li><a href="'+arrData[i].url+'"><strong>'+arrData[i].name+'</strong><span>'+arrData[i].time+'分钟前</span>写了一篇新文章：  '+arrData[i].title+'</a></li>';
+    }
+    //console.log(str);
+    oUl.html(str);
+    iH = oUl.find('li').height();
+    oBtnUp.click(function() {
+      doMove(-1);
+    });
+    oBtnDown.click(function() {
+      doMove(1);
+    });
+    oDiv.hover(function() {
+      clearInterval(timer);
+    }, autoPlay);
+    function autoPlay(){
+      timer = setInterval(function(){
+        doMove(-1);
+      },2500);
+    }
+    autoPlay();
+    function doMove(num){
+      iNow += num;
+      if (Math.abs(iNow)>arrData.length-1) {
+        iNow = 0;
+      }
+      if (iNow>0) {
+        iNow = -(arrData.length-1);
+      }
+      oUl.stop().animate({'top':iNow*iH},500);
+    }
+  })();
+
+  //options选项卡切换
+  (function() {
+    fnTab($('.tabNav1'),$('.tabCon1'),'click');
+    fnTab($('.tabNav2'),$('.tabCon2'),'click');
+    fnTab($('.tabNav3'),$('.tabCon3'),'mouseover');
+    fnTab($('.tabNav4'),$('.tabCon4'),'mouseover');
+
+    function fnTab(iNav,aCon,sEvent) {
+      var iElem = iNav.children();
+      aCon.hide().eq(0).show();
+      iElem.each(function(index) {
+        $(this).on(sEvent,function() {
+          iElem.removeClass('active').addClass('gradient');
+          $(this).removeClass('gradient').addClass('active');
+          iElem.find('a').attr('class','triangle_down_gray');
+          $(this).find('a').attr('class','triangle_down_red');
+          aCon.hide().eq(index).show();
+        });
+      });
+    }
+  })();
+
+
+  //自动播放的焦点图
+  (function(){
+    var oDiv = $('#fade');
+    var aUli = oDiv.find('ul li');
+    var aOli = oDiv.find('ol li');
+    var iNow = 0;
+    var arr = ['爸爸去哪儿啦~', '人像摄影中的光影感', '娇柔妩媚、美艳大方'];
+    var timer = null;
+    fnFade();
+    autoPlay();
+
+    aOli.click(function() {
+      iNow = $(this).index();
+      fnFade();
+    });
+
+    oDiv.hover(function() {
+      clearInterval(timer);
+    }, autoPlay);
+
+    function autoPlay() {
+      timer = setInterval(function(){
+        iNow++;
+        iNow%=arr.length;
+        fnFade();
+      },2000);
+    }
+
+    function fnFade() {
+      aUli.each(function(i) {
+        if (i!=iNow) {
+          aUli.eq(i).fadeOut().css('zIndex',1);
+          aOli.eq(i).removeClass('active');
+        }else {
+          aUli.eq(i).fadeIn().css('zIndex',2);
+          aOli.eq(i).addClass('active');
+        }
+        oDiv.find('p').text(arr[i]);
+      });
+    }
+
+  })();
+
+  //日历提示说明
+  (function(){
+    var aSpan = $('.calendar h3 span');
+    var aImg = $('.calendar img');
+    var oPrompt = $('.today_info');
+    var oImg = oPrompt.find('img');
+    var oStrong = oPrompt.find('strong');
+    var oP = oPrompt.find('p');
+
+    aImg.hover(function() {
+      var iTop = $(this).parent().position().top-30;
+      var iLeft = $(this).parent().position().left+50;
+      var index = $(this).parent().index()%aSpan.size();;
+      console.log(index);
+      oPrompt.show().css({top: iTop,left: iLeft});
+      oP.text($(this).attr('info'));
+      oImg.attr('src',$(this).attr('src'));
+      oStrong.text(aSpan.eq(index).text());
+    }, function() {
+      oPrompt.hide();
+    });
+  })();
+
+  //BBS高亮显示
+  (function(){
+    $('.bbs ol li').mouseover(function(event) {
+      $('.bbs ol li').removeClass('active');
+      $(this).addClass('active');
+    });
+  })();
+
+
+  //HOT鼠标提示效果
+  (function(){
+    var arr = [
+			'',
+			'用户1<br />人气1',
+			'用户名：性感宝贝<br />区域：朝阳CBD<br />人气：124987',
+			'用户3<br />人气3',
+			'用户4<br />人气4',
+			'用户5<br />人气5',
+			'用户6<br />人气6',
+			'用户7<br />人气7',
+			'用户8<br />人气8',
+			'用户9<br />人气9',
+			'用户10<br />人气10'
+		];
+    $('.hot_area li').mouseover(function() {
+      if ($(this).index() == 0) {
+        return;
+      }
+      $('.hot_area li p').remove();
+      //console.log($(this).width());
+      $(this).append('<P style="width:'+($(this).width()-12)+'px;height:'+($(this).height()-12)+'px;">'+arr[$(this).index()]+'</p>')
+    });
+  })();
+
+
+});
